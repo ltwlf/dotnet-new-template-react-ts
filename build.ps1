e7a1dea4e98eb16b5f704bce9c1bc022e96212ac
Christian Glessner <cg@expertsinside.com>
2018-11-07 23:46:18 +0100

    initial commit

---
+++ build.ps1
@@ -0,0 +1,1 @@
+& .\nuget.exe pack ".\templates\AspNetCoreReactTypeScriptTemplate.nuspec" -OutputDirectory ".\Bin"
---
+++ local-install.ps1
@@ -0,0 +1,3 @@
+& .\build.ps1
+
+dotnet new -i ".\Bin\Ltwlf.AspNetCoreTS.0.0.1.nupkg"
